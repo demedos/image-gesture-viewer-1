3635b368ffcc3fbe25c4ae07379dcd0a72545343
Alessandro Diez <alessandro.diez@docebo.com>
2023-09-05 17:38:05 +0200

    adds initial carousel

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
 import React, { Fragment } from 'react';
-import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainApp from './src';
 
 function App() {
   const backgroundStyle = {
-    flex: 1,
-    backgroundColor: '#ececec',
+    backgroundColor: 'black',
   };
 
   return (
     <Fragment>
       <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={'dark-content'} backgroundColor={backgroundStyle.backgroundColor} />
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle} contentContainerStyle={backgroundStyle}>
-          <GestureHandlerRootView style={backgroundStyle}>
-            <MainApp />
-          </GestureHandlerRootView>
-        </ScrollView>
+        <StatusBar barStyle={'light-content'} backgroundColor={backgroundStyle.backgroundColor} />
+        <GestureHandlerRootView style={backgroundStyle}>
+          <MainApp />
+        </GestureHandlerRootView>
       </SafeAreaView>
     </Fragment>
   );
